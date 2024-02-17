97fe85b0f4bff4bd4415b1a4e37ffc5c5e8a4e16
Otchy <id+github@otchy.net>
2024-02-17 21:22:49 +0900

    Install MUI

--- src/pages/Options/index.jsx
+++ src/pages/Options/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import Options from './Options';
 
 const container = document.getElementById('app-container');

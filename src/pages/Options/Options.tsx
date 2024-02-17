77868a5e17bce29e2e557d2b94ea5b83a63d954d
Otchy <id+github@otchy.net>
2024-02-18 01:38:49 +0900

    Reflect bookmark modification immediately

--- src/pages/Options/Options.tsx
+++ src/pages/Options/Options.tsx
@@ -5,14 +5,25 @@ import { BookmarkTreeNode } from '../../types';
 
 const Options: React.FC = () => {
   const [tree, setTree] = useState<BookmarkTreeNode[]>([]);
-  useEffect(() => {
-    (async () => {
+  const resetTree = async () => {
       const root = await chrome.bookmarks.getTree();
       if (root[0]?.children) {
         setTree(root[0].children);
       }
-    })();
-  });
+    };
+  useEffect(() => {
+    resetTree();
+    chrome.bookmarks.onCreated.addListener(resetTree);
+    chrome.bookmarks.onChanged.addListener(resetTree);
+    chrome.bookmarks.onMoved.addListener(resetTree);
+    chrome.bookmarks.onRemoved.addListener(resetTree);
+    return () => {
+      chrome.bookmarks.onCreated.removeListener(resetTree);
+      chrome.bookmarks.onChanged.removeListener(resetTree);
+      chrome.bookmarks.onMoved.removeListener(resetTree);
+      chrome.bookmarks.onRemoved.removeListener(resetTree);
+    }
+  }, []);
   return (
     <>
       <Global

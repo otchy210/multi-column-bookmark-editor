5dcf4de62cfcb9c815e7648fc4dd10d09abaccb6
Otchy <id+github@otchy.net>
2024-03-11 23:13:06 +0900

    Remove unused fonts

--- src/pages/Options/index.jsx
+++ src/pages/Options/index.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 import { Options } from './Options';
 
